ac3b60d8c12608684286a41fd41a7f4892b6fb1e
sahinemin <emin.sahin@agu.edu.tr>
2022-06-25 15:41:44 +0300

    3rd bonus

--- lib/pages/place-details-page.dart
+++ lib/pages/place-details-page.dart
@@ -1,9 +1,12 @@
 
 import 'dart:convert';
+import 'dart:math';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart'as http;
 import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:geolocator/geolocator.dart';
+
 
 class PlaceDetails extends StatefulWidget {
   String accessToken;
@@ -61,7 +64,8 @@ class _PlaceDetailsState extends State<PlaceDetails> {
                         padding: const EdgeInsets.all(10.0),
                         child: Text(snapshot.data![0]["full"], style: const TextStyle(color: Colors.black, fontSize: 18,)
                         ),
-                      )
+                      ),
+                    Text("Distance: $distance km",style:const TextStyle(fontSize: 18),)
                   ],
                 ),
               );
@@ -79,6 +83,9 @@ class _PlaceDetailsState extends State<PlaceDetails> {
   }
 }
 List<String> list=[];
+double latitude=0.0;
+double longitude=0.0;
+double distance=0.0;
 Future<List<dynamic>?> fetchData(String token,city,bool hasConnection,BuildContext context) async {
   hasConnection = await InternetConnectionChecker().hasConnection;
   if(hasConnection) {
@@ -91,6 +98,10 @@ Future<List<dynamic>?> fetchData(String token,city,bool hasConnection,BuildConte
     var response = await http.post(
         url, headers: headers, body: '{"city":"$city"}');
     var decoded = json.decode(response.body);
+    latitude=decoded[0]["latitude"];
+    longitude=decoded[0]["longitude"];
+
+
     //print(decoded[0]["landing"]);
     //print(decoded[0]["alternative"].toString().substring(1,decoded[0]["alternative"].toString().length-1).split(",").length);
     list = [];
@@ -112,8 +123,8 @@ Future<List<dynamic>?> fetchData(String token,city,bool hasConnection,BuildConte
             .length - 1).split(",")[i].trim());
       }
     }
-
-    // print(list);
+    Position position= await _determinePosition();
+    distance = Geolocator.distanceBetween(position.latitude, position.longitude, decoded[0]["latitude"],decoded[0]["longitude"]).ceil()/1000;
 
     return decoded;
   }
@@ -121,4 +132,42 @@ Future<List<dynamic>?> fetchData(String token,city,bool hasConnection,BuildConte
     ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Network is not available")));
     return null;
   }
+}
+
+Future<Position> _determinePosition() async {
+  bool serviceEnabled;
+  LocationPermission permission;
+
+  // Test if location services are enabled.
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    // Location services are not enabled don't continue
+    // accessing the position and request users of the
+    // App to enable the location services.
+    return Future.error('Location services are disabled.');
+  }
+
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      // Permissions are denied, next time you could try
+      // requesting permissions again (this is also where
+      // Android's shouldShowRequestPermissionRationale
+      // returned true. According to Android guidelines
+      // your App should show an explanatory UI now.
+      return Future.error('Location permissions are denied');
+    }
+  }
+
+  if (permission == LocationPermission.deniedForever) {
+    // Permissions are denied forever, handle appropriately.
+    return Future.error(
+        'Location permissions are permanently denied, we cannot request permissions.');
+  }
+
+  // When we reach here, permissions are granted and we can
+  // continue accessing the position of the device.
+
+  return await Geolocator.getCurrentPosition();
 }
